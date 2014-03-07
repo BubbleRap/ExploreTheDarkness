7cbe021a92cb5d225b6f7651e68234e826f21396
Xroft666 <vapyhtin@gmail.com>
2014-03-07 16:35:58 +0100

    shadows scene and standart assets

---
+++ UnityProject/Assets/Scripts/LuminocityIE/LuminocityCalc.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LuminocityCalc : MonoBehaviour 
+{
+	public RenderTexture[] textures;
+
+	void Start()
+	{
+		print (RenderTexture.SupportsStencil (textures [0]));
+	}
+
+	void Update () 
+	{
+//		float totalLuminocity = 0f;
+//
+//		foreach (RenderTexture tex in textures) 
+//		{
+//			tex.
+//		}
+	}
+}
