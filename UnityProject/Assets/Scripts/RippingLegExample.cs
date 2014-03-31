d8077f987cf56f0ec3557ac17dee7b9dca8fef90
Victor Apihtin <vapyhtin@gmail.com>
2014-03-31 23:52:44 +0200

    Lil bro ragdoll

---
+++ UnityProject/Assets/Scripts/RippingLegExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RippingLegExample : MonoBehaviour 
+{
+	CameraFade fader = null;
+
+	void Start()
+	{
+		fader = Camera.main.GetComponent<CameraFade>();
+	}
+
+	void Update()
+	{
+		if( fader.fadeIntensity > 0.3f )
+			rigidbody.AddForce(fader.camera.transform.forward , ForceMode.Acceleration);
+	}
+}
