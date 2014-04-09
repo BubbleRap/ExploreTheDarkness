34b11ceab4e0b3ddf88f9cea7f494bf711ab7c71
Miki Anthony <miki_anthony@hotmail.com>
2014-04-09 12:30:49 +0200

    Interact with objects and sound plays

--- UnityProject/Assets/Scripts/interact.cs
+++ UnityProject/Assets/Scripts/interact.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class interact : MonoBehaviour {
 
 	public Transform firstPersonCamera;
+	public Transform thirdPersonCamera;
+	bool isFirstPerson = false;
 	bool isInteractMode = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isInteractMode && Input.GetKeyDown(KeyCode.E))
 		{
-			firstPersonCamera.gameObject.SetActive(false);
-			transform.gameObject.GetComponent<ThirdPersonController>().canMove = true;
+			if(isFirstPerson)
+			{
+				firstPersonCamera.gameObject.SetActive(false);
+			}
+			transform.gameObject.GetComponent<MovementController>().canMove = true;
 			isInteractMode = false;
 			/*
 			foreach (Transform child in transform){
@@ -29,9 +34,9 @@ public class interact : MonoBehaviour {
 		}
 		else
 		{
-			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			Vector3 fwd = thirdPersonCamera.TransformDirection(Vector3.forward);
 			RaycastHit[] hits;
-			hits = Physics.RaycastAll(transform.position, fwd, 10);
+			hits = Physics.RaycastAll(thirdPersonCamera.position, fwd, 10);
 			int i = 0;
 			while (i < hits.Length) {
 
@@ -40,19 +45,30 @@ public class interact : MonoBehaviour {
 				{
 					if(Input.GetKeyDown(KeyCode.E))
 					{
-						firstPersonCamera.gameObject.SetActive(true);
-						firstPersonCamera.LookAt(hit.transform);
-						transform.gameObject.GetComponent<ThirdPersonController>().canMove = false;
-						//Destroy(hit.transform.gameObject);
-						isInteractMode = true;
-						/*
-						foreach (Transform child in transform){
-							if(child.tag == "Player")
+						if(hit.transform.GetComponent<Object>() != null)
+						{
+							if(hit.transform.GetComponent<Object>().firstperson)
 							{
-								child.gameObject.SetActive(false);
+								firstPersonCamera.gameObject.SetActive(true);
+								isFirstPerson = true;
+								firstPersonCamera.LookAt(hit.transform);
+								transform.gameObject.GetComponent<MovementController>().canMove = false;
+								//Destroy(hit.transform.gameObject);
+								isInteractMode = true;
+								/*
+								foreach (Transform child in transform){
+									if(child.tag == "Player")
+									{
+										child.gameObject.SetActive(false);
+									}
+								}
+								*/
+							}
+							if(hit.transform.GetComponent<Object>().soundClip != null)
+							{
+								hit.transform.GetComponent<Object>().PlayAudio();
 							}
 						}
-						*/
 					}
 				}
 				i++;
