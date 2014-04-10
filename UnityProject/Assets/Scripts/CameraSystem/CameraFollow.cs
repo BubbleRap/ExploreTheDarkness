6f210dc47b7a27e9eabe9119f3a7cf41c125a138
Miki Anthony <miki_anthony@hotmail.com>
2014-04-10 14:35:21 +0200

    CameraShake transition (Light->Shadow)

--- UnityProject/Assets/Scripts/CameraSystem/CameraFollow.cs
+++ UnityProject/Assets/Scripts/CameraSystem/CameraFollow.cs
@@ -41,6 +41,8 @@ public class CameraFollow : MonoBehaviour
 
 	// private sector, dude.
 	// Authorized personel only!
+	// **** Access Granted **** 
+	// You may proceed ...
 	private Vector3 shakeOffset;
 
 	private void Start()
@@ -71,8 +73,8 @@ public class CameraFollow : MonoBehaviour
 				}
 				else
 				{
-					relativePosition = GetVectorFromAngle(pitch, yaw, roll, cameraDistance, cameraFocusTarget.position);
-					transform.position = Vector3.Lerp(transform.position, /*cameraFocusTarget.TransformPoint(shakeOffset) + */relativePosition, followingSpeed);
+					relativePosition = GetVectorFromAngle(pitch - shakeOffset.y, yaw - shakeOffset.x, roll, cameraDistance + shakeOffset.z, cameraFocusTarget.position);
+					transform.position = Vector3.Lerp(transform.position, relativePosition, followingSpeed);
 				}
 
 			}
@@ -96,8 +98,8 @@ public class CameraFollow : MonoBehaviour
 	{
 		while (true) 
 		{
-			shakeOffset = new Vector3( Random.Range(-horizontalShakeIntensity, horizontalShakeIntensity), 
-					                          Random.Range(-verticalShakeIntensity, verticalShakeIntensity), 0f);
+			shakeOffset = new Vector3( Random.Range(-horizontalShakeIntensity * 10, horizontalShakeIntensity * 10), 
+					                          Random.Range(-verticalShakeIntensity * 10, verticalShakeIntensity * 10), 0f);
 			yield return new WaitForSeconds(shakeFrequency);
 		}
 	}
