b116be1de1a84f5e8f86a341ca9e6910c7a62937
Victor Apihtin <vapyhtin@gmail.com>
2014-04-11 21:40:45 +0200

    a little smarter ai

---
+++ UnityProject/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class AIBehaviour : MonoBehaviour 
+{
+	private AIRig aiMind = null;
+
+	public void Awake()
+	{
+		aiMind = GetComponentInChildren<AIRig>();
+	}
+
+	public void RetriveLightProbeResult(float intensity)
+	{
+		aiMind.AI.WorkingMemory.SetItem("lightIntensity", intensity);
+	}
+}
