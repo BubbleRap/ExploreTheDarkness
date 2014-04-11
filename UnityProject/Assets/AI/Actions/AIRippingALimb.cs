a61a5cadb230ea7c40c58458746a0d67503eeeec
Xroft666 <vapyhtin@gmail.com>
2014-04-11 17:00:46 +0200

    few fixes and AI ripping code mocap

---
+++ UnityProject/Assets/AI/Actions/AIRippingALimb.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Core;
+using RAIN.Action;
+using RAIN.Representation;
+
+[RAINAction]
+public class AIRippingALimb : RAINAction
+{
+	public Expression characterForm;
+	private SiljaBehaviour silja = null;
+	private GameObject siljaGO = null;
+
+    public AIRippingALimb()
+    {
+        actionName = "AIRippingALimb";
+    }
+
+    public override void Start(AI ai)
+    {
+		siljaGO = characterForm.Evaluate (ai.DeltaTime, ai.WorkingMemory).GetValue<GameObject> ();
+		if (siljaGO != null)
+			silja = siljaGO.GetComponent<SiljaBehaviour> ();
+		else
+			Debug.LogWarning ("Silja hasnt been found");
+
+        base.Start(ai);
+
+    }
+
+    public override ActionResult Execute(AI ai)
+    {
+		if (silja == null)
+			return ActionResult.FAILURE;
+
+		silja.TakeALimb ();
+
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(AI ai)
+    {
+        base.Stop(ai);
+    }
+}
---
+++ UnityProject/Assets/Scripts/SiljaBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SiljaBehaviour : MonoBehaviour 
+{
+	private int lives = 5;
+
+	public void TakeALimb()
+	{
+		lives--;
+		print ("Took a limb. " + lives + " limbs left");
+	}
+}
