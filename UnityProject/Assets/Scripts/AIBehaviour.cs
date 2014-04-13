4c043b3e9cc6238f671fbaf8652c66bb81a55e5e
Victor Apihtin <vapyhtin@gmail.com>
2014-04-13 18:37:52 +0200

    a small reconstruction

--- UnityProject/Assets/Scripts/AIBehaviour.cs
+++ UnityProject/Assets/Scripts/AIBehaviour.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using RAIN.Core;
+using RAIN.Navigation;
+using RAIN.Navigation.Targets;
 
 public class AIBehaviour : MonoBehaviour 
 {
 	private AIRig aiMind = null;
 
-	public void Awake()
+	public string[] spawnPoints;
+	private GameObject renderer = null;
+
+	void Awake()
 	{
-		aiMind = GetComponentInChildren<AIRig>();
+		renderer = transform.FindChild("Renderer").gameObject;
+	}
+	
+	public void SpawnAI()
+	{
+		NavigationTarget spawnPoint = NavigationManager.instance.GetNavigationTarget(spawnPoints[ Random.Range(0, spawnPoints.Length) ]);
+		transform.position = spawnPoint.Position;
+		aiMind.enabled = true;
+		renderer.SetActive(true);
+	}
+
+	public void DespawnAI()
+	{
+		aiMind.enabled = false;
+		renderer.SetActive(false);
 	}
 
 	public void RetriveLightProbeResult(float intensity)
