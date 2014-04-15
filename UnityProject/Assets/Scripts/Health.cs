7417f8054578dbc449f27b9b36cbf4ab25431c42
Miki Anthony <miki_anthony@hotmail.com>
2014-04-15 16:13:32 +0200

    Playtest 002 - "battery bar"

--- UnityProject/Assets/Scripts/Health.cs
+++ UnityProject/Assets/Scripts/Health.cs
@@ -5,6 +5,19 @@ public class Health : MonoBehaviour {
 
 	public int health = 8;
 	public Transitioner transitionController;
+	public ShadowModeController shadowController;
+
+	private float barDisplay = 0;
+	private Vector2 pos = new Vector2(Screen.width/2-400,Screen.height-50);
+	private Vector2 size = new Vector2(800,20);
+	private Texture2D progressBarEmpty;
+	private Texture2D progressBarFull;
+
+	private GUIStyle currentStyle = new GUIStyle();
+	public Texture2D texture;
+
+	private GUIStyle currentStyle2 = new GUIStyle();
+	public Texture2D texture2;
 
 	// Use this for initialization
 	void Start () {
@@ -17,12 +30,26 @@ public class Health : MonoBehaviour {
 		{
 			GUI.skin.label.fontSize = 100;
 			GUI.Label(new Rect (40,20,100,300), health.ToString());
+
+        	currentStyle.normal.background = texture;
+        	currentStyle2.normal.background = texture2;
+
+
+			GUI.BeginGroup (new Rect (pos.x, pos.y, size.x, size.y));
+		        GUI.Box (new Rect (0,0, size.x, size.y),progressBarEmpty, currentStyle);
+		 
+		        // draw the filled-in part:
+		        GUI.BeginGroup (new Rect (0, 0, size.x * (barDisplay/1.75f), size.y));
+		            GUI.Box (new Rect (0,0, size.x, size.y),progressBarFull, currentStyle2);
+		        GUI.EndGroup ();
+	 
+	    	GUI.EndGroup ();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		barDisplay = shadowController.getTeddyLight();
 	}
 
 	public void looseLife() {
