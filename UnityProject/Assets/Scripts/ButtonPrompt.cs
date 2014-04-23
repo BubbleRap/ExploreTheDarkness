3a643bf1738cc134b9237495f6d87e0300dbe8d2
Michal Krolikowski <hiszpanskainkwizycja@gmail.com>
2014-04-23 17:03:04 +0200

    Fix 'e' prompt

---
+++ UnityProject/Assets/Scripts/ButtonPrompt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPrompt : MonoBehaviour {
+
+	public HighlightedObject highlightedObject;
+	public float distance = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 desiredPosition = new Vector3(highlightedObject.transform.position.x,
+		                                      highlightedObject.transform.position.y,
+		                                      highlightedObject.transform.position.z);
+
+		Vector3 modifier = Camera.main.gameObject.transform.position - desiredPosition;
+		modifier.Normalize ();
+		modifier *= distance;
+
+		transform.position = desiredPosition + modifier;
+
+		transform.LookAt(Camera.main.gameObject.transform);
+	}
+}
