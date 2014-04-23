6e901d0576897753b1a05e3a3a7791db4a5da5bf
Victor Apihtin <vapyhtin@gmail.com>
2014-04-23 20:37:26 +0200

    editor script for materials

---
+++ UnityProject/Assets/Scripts/Editor/MaterialsAlphaUtility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class MaterialsAlphaUtility : Editor 
+{
+	[MenuItem("Utility/Set All Alphas To Zero")]
+	static public void SetMaterialsAlphaToZero()
+	{
+
+	}
+
+	[MenuItem("Utility/Set All Alphas To One")]
+	static public void SetMaterialsAlphaToOne()
+	{
+		
+	}
+}
