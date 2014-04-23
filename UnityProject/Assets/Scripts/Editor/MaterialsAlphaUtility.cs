7d557c98ec6769a66ef1b138c0e2fccac84ef52d
Victor Apihtin <vapyhtin@gmail.com>
2014-04-23 21:14:13 +0200

    super awesome utility function

--- UnityProject/Assets/Scripts/Editor/MaterialsAlphaUtility.cs
+++ UnityProject/Assets/Scripts/Editor/MaterialsAlphaUtility.cs
@@ -1,18 +1,60 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
+using System.IO;
 
 public class MaterialsAlphaUtility : Editor 
 {
 	[MenuItem("Utility/Set All Alphas To Zero")]
 	static public void SetMaterialsAlphaToZero()
 	{
+		string[] materialsInProject = Directory.GetFiles( "Assets\\", "*.mat", SearchOption.AllDirectories );
 
+		foreach( string materialAssetPath in materialsInProject )
+		{
+			Material targetMaterial = (Material)AssetDatabase.LoadAssetAtPath( materialAssetPath.Replace( "\\", "/" ), typeof(Material) );
+			string propertyName = "";
+
+			if( targetMaterial.HasProperty( "_Tint" ) )
+			   propertyName = "_Tint";
+			
+			if( targetMaterial.HasProperty( "_Color" ) )
+				propertyName = "_Color";
+
+			if( string.IsNullOrEmpty(propertyName) )
+				continue;
+
+			Color materialColor = targetMaterial.GetColor(propertyName);
+
+			materialColor.a = 0f;
+
+			targetMaterial.SetColor(propertyName, materialColor );
+		}
 	}
 
 	[MenuItem("Utility/Set All Alphas To One")]
 	static public void SetMaterialsAlphaToOne()
-	{
+	{		
+		string[] materialsInProject = Directory.GetFiles( "Assets\\", "*.mat", SearchOption.AllDirectories );
 		
+		foreach( string materialAssetPath in materialsInProject )
+		{
+			Material targetMaterial = (Material)AssetDatabase.LoadAssetAtPath( materialAssetPath.Replace( "\\", "/" ), typeof(Material) );
+			string propertyName = "";
+			
+			if( targetMaterial.HasProperty( "_Tint" ) )
+				propertyName = "_Tint";
+			
+			if( targetMaterial.HasProperty( "_Color" ) )
+				propertyName = "_Color";
+			
+			if( string.IsNullOrEmpty(propertyName) )
+				continue;
+			
+			Color materialColor = targetMaterial.GetColor(propertyName);
+			
+			materialColor.a = 1f;
+			
+			targetMaterial.SetColor(propertyName, materialColor );
+		}
 	}
 }
