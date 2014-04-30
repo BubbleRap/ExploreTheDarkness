bc85c67ad41ffba3dc5a956d7d0ace3837fc865c
Miki Anthony <miki_anthony@hotmail.com>
2014-04-30 11:55:11 +0200

    Animation Trigger fix

---
+++ UnityProject/Assets/Scripts/Trigger_animation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Trigger_animation : MonoBehaviour {
+
+	private Transitioner transitionContoller = null;
+
+	public List<Transform> animationsToTrigger;
+	public List<float> animationDelays;
+
+	// Use this for initialization
+	void Awake () {
+		transitionContoller = Component.FindObjectOfType(typeof(Transitioner)) as Transitioner;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player")
+		{
+			for (int i=0; i<animationsToTrigger.Count; ++i){
+				StartCoroutine("DelayAndPlay",i);
+			}
+
+			collider.enabled = false;
+		}
+	}
+	
+	public IEnumerator DelayAndPlay(int index){
+		if (animationDelays.Count > index)
+			yield return new WaitForSeconds(animationDelays[index]);
+
+		animationsToTrigger[index].animation.Play();
+
+		if(animationsToTrigger[index].GetComponent<AudioSource>() != null)
+			animationsToTrigger[index].GetComponent<AudioSource>().Play();
+	}
+}
