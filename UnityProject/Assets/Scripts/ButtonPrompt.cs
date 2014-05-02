87ad308ac5de867060c0671ef9669fcd62b0bd53
Victor Apihtin <vapyhtin@gmail.com>
2014-05-02 20:10:41 +0200

    e button + glow highlights

--- UnityProject/Assets/Scripts/ButtonPrompt.cs
+++ UnityProject/Assets/Scripts/ButtonPrompt.cs
@@ -6,19 +6,29 @@ public class ButtonPrompt : MonoBehaviour {
 	public HighlightedObject highlightedObject;
 	public float distance = 0.5f;
 
+	public Camera hudCamera;
+
+	void Start()
+	{
+		//hudCamera = GameObject.FindGameObjectWithTag("HUDCamera").camera;
+		hudCamera = Camera.main;
+	}
+
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 desiredPosition = new Vector3(highlightedObject.transform.position.x,
-		                                      highlightedObject.transform.position.y,
-		                                      highlightedObject.transform.position.z);
+		//Vector3 desiredPosition = new Vector3(highlightedObject.transform.position.x,
+		//                                      highlightedObject.transform.position.y,
+		//                                      highlightedObject.transform.position.z);
+		//
+		//Vector3 modifier = hudCamera.gameObject.transform.position - desiredPosition;
+		//modifier.Normalize ();
+		//modifier *= distance;
+		//
+		//transform.position = desiredPosition + modifier;
 
-		Vector3 modifier = Camera.main.gameObject.transform.position - desiredPosition;
-		modifier.Normalize ();
-		modifier *= distance;
-
-		transform.position = desiredPosition + modifier;
-
-		transform.LookAt(Camera.main.gameObject.transform);
+		float size = (hudCamera.gameObject.transform.position - transform.position).magnitude ;
+		transform.localScale = new Vector3(size,size,size);
+		transform.LookAt(hudCamera.gameObject.transform);
 	}
 }
