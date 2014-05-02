dbf46f6626435a4022e22cb09d00728ada59de9c
Victor Apihtin <vapyhtin@gmail.com>
2014-05-02 20:10:41 +0200

    e button + glow highlights

--- UnityProject/Assets/Scripts/HighlightedObject.cs
+++ UnityProject/Assets/Scripts/HighlightedObject.cs
@@ -15,6 +15,8 @@ public class HighlightedObject : MonoBehaviour {
 
 	private bool activated = false;
 
+	private interact interactionScript = null;
+
 	void Start () {
 
 		if(internalPlay)
@@ -29,9 +31,22 @@ public class HighlightedObject : MonoBehaviour {
 		buttonPrompt = Instantiate(Resources.Load<GameObject>("buttonPrompt"), new Vector3(transform.position.x + 0.3f, transform.position.y + 0.2f, transform.position.z + 0.2f), transform.rotation) as GameObject;
 		buttonPrompt.GetComponent<ButtonPrompt> ().highlightedObject = this;
 		buttonPrompt.SetActive(false);
+
+		interactionScript = FindObjectOfType(typeof(interact)) as interact;
 	}
 
 	void Update () {
+
+		if( !renderer.isVisible )
+			return;
+
+		Vector3 cameraRelativePosition = Camera.main.transform.InverseTransformPoint(transform.position);
+		if( cameraRelativePosition.x < 0.5f * cameraRelativePosition.z && cameraRelativePosition.x > -0.5f * cameraRelativePosition.z )
+			hitObject = true;
+		else
+			hitObject = false;
+
+
 		if(hitObject)
 		{
 			if(!activated)
@@ -39,9 +54,6 @@ public class HighlightedObject : MonoBehaviour {
 				Color curColor = renderer.material.color;
 				curColor.a = 1f;
 				renderer.material.color = curColor;
-
-				buttonPrompt.SetActive(true);
-				buttonPrompt.transform.LookAt(Camera.main.gameObject.transform);
 
 				activated = true;
 			}
@@ -54,11 +66,31 @@ public class HighlightedObject : MonoBehaviour {
 				curColor.a = 0f;
 				renderer.material.color = curColor;
 
-				buttonPrompt.SetActive(false);
-
 				activated = false;
 			}
 		}
+
+		if( (transform.position - Camera.main.transform.position).magnitude < 3f && activated)
+		{
+			if( !buttonPrompt.activeInHierarchy )
+			{
+				buttonPrompt.SetActive(true);
+				buttonPrompt.transform.LookAt(Camera.main.gameObject.transform);
+
+				interactionScript.interactedObject = this;
+			}
+		}
+		else 
+		{
+			if( buttonPrompt.activeInHierarchy )
+			{
+				buttonPrompt.SetActive(false);
+
+				interactionScript.interactedObject = null;
+			}
+		}
+
+
 		hitObject = false;
 	}
 
