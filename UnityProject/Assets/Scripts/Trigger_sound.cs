8bc4724db43e3352fc48e86db74b11b5748bec42
Victor Apihtin <vapyhtin@gmail.com>
2014-05-11 14:18:57 +0200

    trigger script changed to static call

--- UnityProject/Assets/Scripts/Trigger_sound.cs
+++ UnityProject/Assets/Scripts/Trigger_sound.cs
@@ -12,33 +12,32 @@ public class Trigger_sound : MonoBehaviour {
 	private bool isTrigger = false;
 	// Use this for initialization
 	void Start () {
-		thirdPersonCamera = Camera.main.transform;
+	//	thirdPersonCamera = Camera.main.transform;
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Player")
 		{
-			audioSource = soundSource.GetComponent<AudioSource>();
-			audioSource2 = thirdPersonCamera.gameObject.GetComponent<AudioSource>();
-			if(audioSource == null)
-			{
-				audioSource = audioSource2;
-			}
+			//audioSource = soundSource.GetComponent<AudioSource>();
+			//audioSource2 = thirdPersonCamera.gameObject.GetComponent<AudioSource>();
+			//if(audioSource == null)
+			//{
+			//	audioSource = audioSource2;
+			//}
 
-			if(isTrigger && !playMultipleTimes)
-			return;
+			//if(isTrigger && !playMultipleTimes)
+			//return;
+			//
+			//	if(!audioSource2.isPlaying && !audioSource.isPlaying)
+			//	{
+			//		//audioSource.clip = audioClip;
+			//		//audioSource.Play();
+			//		
+			//		isTrigger = true;
+			//	}
 
-				if(!audioSource2.isPlaying && !audioSource.isPlaying)
-				{
-					audioSource.clip = audioClip;
-					audioSource.Play();
-					isTrigger = true;
-				}
+			AudioSource.PlayClipAtPoint(audioClip, transform.position);
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
