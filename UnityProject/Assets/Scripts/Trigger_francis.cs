ad389c98231ff493a8ac396ee33943a3ee7cf6a0
Miki Anthony <miki_anthony@hotmail.com>
2014-05-13 00:57:29 +0200

    Trigger Francis script (turn lillebror off)

---
+++ UnityProject/Assets/Scripts/Trigger_francis.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_francis : MonoBehaviour {
+
+	public SiljaBehaviour siljaBeh;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player")
+		{
+			siljaBeh.SetLightIntensity(0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
