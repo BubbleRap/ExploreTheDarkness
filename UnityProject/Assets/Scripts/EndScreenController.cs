767f8152d99175b7dd4e7800d403bb0512191bf9
SG1990 <sgsbox90@gmail.com>
2014-05-13 15:03:11 +0200

    Nicer fade into end screen

--- UnityProject/Assets/Scripts/EndScreenController.cs
+++ UnityProject/Assets/Scripts/EndScreenController.cs
@@ -8,7 +8,20 @@ public class EndScreenController : MonoBehaviour {
 
 	public GameObject VictorScreen;
 	public GameObject GameOverScreen;
-	
+	private bool victor;
+
+	public Color fadeColor = Color.black;
+	private Texture2D fadeTexture = null;
+	private float currentAlpha = 0f;
+	public float fadeTime = 2f;
+
+	void Awake()
+	{
+		fadeTexture = new Texture2D(1,1, TextureFormat.RGBA32, false, false);
+		fadeTexture.SetPixel(0,0, fadeColor);
+		fadeTexture.Apply();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
@@ -19,20 +32,56 @@ public class EndScreenController : MonoBehaviour {
 		}
 	}
 	
-	public void ShowEndScreen(bool victor){
-		EndScreenActive = true;
-		EndScreenCamera.enabled = true;
+	public void ShowEndScreen(bool avictor){
+
+		victor = avictor;
+		Time.timeScale = 0.0001f;
+		foreach (AudioSource audio in GameObject.FindObjectsOfType<AudioSource>()){
+			if (audio.time != 0f){
+				audio.Pause();
+			}
+		}
+
+		StartCoroutine("FadeToEndScreen");
+	}
+
+	private IEnumerator FadeToEndScreen(){
+
+		fadeTime *= 0.0001f;
+		float fadeStart = Time.time;
+
+		while (Time.time < fadeStart + fadeTime){
+			currentAlpha = (Time.time - fadeStart)/fadeTime;
+			yield return null;
+		}
 
 		if (victor)
 			VictorScreen.SetActive(true);
 		else
 			GameOverScreen.SetActive(true);
 
-		Time.timeScale = 0.0001f;
-		foreach (AudioSource audio in GameObject.FindObjectsOfType<AudioSource>()){
-			if (audio.time != 0f){
-				audio.Pause();
-			}
+		EndScreenActive = true;
+		EndScreenCamera.enabled = true;
+
+		fadeStart = Time.time;
+		
+		while (Time.time < fadeStart + fadeTime){
+			currentAlpha = 1f - (Time.time - fadeStart)/fadeTime;
+			yield return null;
+		}
+
+		currentAlpha = 0f;
+	}
+
+	private void OnGUI() 
+	{ 
+		if(currentAlpha > 0f)
+		{
+			Color guiColor = fadeColor;
+			guiColor.a = currentAlpha;
+			GUI.color = guiColor;
+		
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),fadeTexture);
 		}
 	}
 }
