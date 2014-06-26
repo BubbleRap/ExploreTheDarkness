423d1912f055f8caeb93d06f1f3b7bf181f110ef
Miki Anthony <miki_anthony@hotmail.com>
2014-06-26 16:26:42 +0200

    Francis spawn zone (loose 1 life), ambientLight connected with TeddyLight source.

--- UnityProject/Assets/Scripts/Triggers/Trigger_francis.cs
+++ UnityProject/Assets/Scripts/Triggers/Trigger_francis.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Trigger_francis : MonoBehaviour {
 
 	public SiljaBehaviour siljaBeh;
+	public Health healthController;
 	private AIBehaviour[] aiEntities = null;
+	private int health;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +17,31 @@ public class Trigger_francis : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
+		health = healthController.health;
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(other.tag == "Player")
+		{
+			if(siljaBeh.getTeddyLight() <= siljaBeh.getMimimumIntensity())
+			{
+				foreach( AIBehaviour aiEntity in aiEntities )
+					aiEntity.SpawnAI();
+
+			}
+
+			if(health > healthController.health)
+			{
+				Destroy(this);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
 		if(other.tag == "Player")
 		{
 			foreach( AIBehaviour aiEntity in aiEntities )
-				aiEntity.SpawnAI();
-
-			siljaBeh.SetLightIntensity(0f);
-			Destroy(this);
+				aiEntity.DespawnAI();
 		}
 	}
 	
