e3b8753ecdb09a1aecb1ae7836d53369f9ac6e75
Victor Apihtin <vapyhtin@gmail.com>
2015-01-24 02:49:52 +0100

    Camera Collision solver:
    raycasts to the screen center
    raycasts to the character
    fallback movement if no collision

--- UnityProject/Assets/Scripts/CameraSystem/CameraPhysics.cs
+++ UnityProject/Assets/Scripts/CameraSystem/CameraPhysics.cs
@@ -12,9 +12,16 @@ public class CameraPhysics : MonoBehaviour
 	[Range(0f, 1f)]
 	public float approachingSpeed = 0.1f;
 
+	public AnimationCurve fallbackCurve;
+	[Range(0f, 5f)]
+	public float fallbackTime = 5f;
+
 	private CameraFollow follower = null;
 	private float originalDistance = 0f;
 	private List<Collider> colliders = new List<Collider>();
+
+	private float _timer = 0f;
+	private float fallbackFrom = 0f;
 
 	void Awake()
 	{
@@ -24,13 +31,35 @@ public class CameraPhysics : MonoBehaviour
 
 	void Update()
 	{
-		if( colliders.Count > 0 || Physics.Raycast(transform.position, -transform.forward, 1f, 1 << 0 ) )
+		_timer += Time.deltaTime;
+		_timer = Mathf.Clamp(_timer, 0f, fallbackTime);
+
+//		RaycastHit hit = default(RaycastHit);
+
+		// if camera collides itself
+		if( colliders.Count > 0 
+		   // if camera center point raycast hits a wall
+		   ||  	Physics.Raycast(transform.position, transform.forward, transform.localPosition.magnitude, 1 << 0 ) 
+		   // if raycast to character hits a wall
+		   ||	Physics.Raycast(transform.position, (follower.cameraFocusTarget.position - transform.position).normalized/*, out hit*/, follower.cameraDistance, 1 << 0) 
+		   )
+		{
 			follower.cameraDistance = Mathf.Lerp (follower.cameraDistance, originalDistance * minimumDistance, approachingSpeed);
+
+//			Debug.Log(hit.collider.gameObject.name);
+
+			_timer = 0f;
+			fallbackFrom = follower.cameraDistance;
+		}
 		else
 		{
-			if( !Physics.Raycast(transform.position, -transform.forward, 1f ))
-				follower.cameraDistance = Mathf.Lerp (follower.cameraDistance, originalDistance, approachingSpeed);
+			if( _timer < fallbackTime )
+			{
+	//			Debug.Log(_timer);
+				follower.cameraDistance = Mathf.Lerp (fallbackFrom, originalDistance, fallbackCurve.Evaluate(_timer / fallbackTime ) );
+			}
 		}
+
 	}
 
 	void OnTriggerEnter(Collider other)
