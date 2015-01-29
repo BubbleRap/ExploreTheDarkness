33a59ece3a630145b73fb4ec43e5a59de42fe622
Miki Anthony <miki_anthony@hotmail.com>
2015-01-29 14:51:54 +0100

    Objectives in UI and sewing puzzle.

---
+++ UnityProject/Assets/Scripts/Interaction System/SewLillebror.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SewLillebror :IInteractableObject 
+{
+
+	public override void Activate()
+	{
+		interactionIsActive = !interactionIsActive;
+
+		GameObject siljaGO = GameObject.FindGameObjectWithTag("Player");
+		Inventory siljaInventory = siljaGO.GetComponent<Inventory>();
+		if(siljaGO.GetComponent<Inventory>().getLilleBrorPieces() > 4)
+		{
+			if(siljaGO.GetComponent<CollectLillebrorUI>() == null)
+			{
+				CollectLillebrorUI objectiveController = siljaGO.AddComponent<CollectLillebrorUI>();
+			}
+			siljaGO.GetComponent<CollectLillebrorUI>().lillebrorMessage = "";
+			siljaGO.GetComponent<CollectLillebrorUI>().multipleTask = false;
+
+			siljaInventory.lillebrorComplete = true;
+		}
+    }
+}
---
+++ UnityProject/Assets/Scripts/UI/CollectLillebrorUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectLillebrorUI : MonoBehaviour {
+
+	public string lillebrorMessage;
+	public bool multipleTask = false;
+	private int lilleBrorPiecesTotal = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject siljaGO = GameObject.FindGameObjectWithTag("Player");
+		Objectives siljaObjective = siljaGO.GetComponent<Objectives>();
+		Inventory siljaInventory = siljaGO.GetComponent<Inventory>();
+		
+		if(multipleTask)
+		{
+			siljaObjective.setTheObjective(lillebrorMessage + " " + siljaInventory.getLilleBrorPieces() + "/" + lilleBrorPiecesTotal);
+		}
+		else
+		{
+			siljaObjective.setTheObjective(lillebrorMessage);
+		}
+	}
+}
