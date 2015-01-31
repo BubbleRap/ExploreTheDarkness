90e536c69387d79f9a3d0a6177f7c2d754e93d4d
Miki Anthony <miki_anthony@hotmail.com>
2015-01-31 14:56:45 +0100

    Playing (dummy) sounds in puzzle

--- UnityProject/Assets/Scripts/Interaction System/SewLillebror.cs
+++ UnityProject/Assets/Scripts/Interaction System/SewLillebror.cs
@@ -10,8 +10,16 @@ public class SewLillebror :IInteractableObject
 
 		GameObject siljaGO = GameObject.FindGameObjectWithTag("Player");
 		Inventory siljaInventory = siljaGO.GetComponent<Inventory>();
-		if(siljaGO.GetComponent<Inventory>().getLilleBrorPieces() > 4)
+		if(siljaGO.GetComponent<Inventory>().getLilleBrorPieces() > 4 && !siljaGO.GetComponent<Inventory>().lillebrorComplete)
 		{
+			if(GameObject.Find("PuzzleTrigger") != null)
+			{
+				AudioClip audioClip = GameObject.Find("PuzzleTrigger").GetComponent<PuzzleLillebror>().sewingLillebrorSound;
+				AudioSource soundSource = GameObject.Find("HeadAudioSource").transform.audio;
+				soundSource.clip = audioClip;
+				soundSource.Play();
+			}
+
 			if(siljaGO.GetComponent<CollectLillebrorUI>() == null)
 			{
 				CollectLillebrorUI objectiveController = siljaGO.AddComponent<CollectLillebrorUI>();
--- UnityProject/Assets/Scripts/Triggers/PuzzleLillebror.cs
+++ UnityProject/Assets/Scripts/Triggers/PuzzleLillebror.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class PuzzleLillebror : MonoBehaviour {
 
 	public DoorOpenController Door;
+	public AudioClip[] needLillebrorSound;
+	public AudioClip[] collectLillebrorSounds;
+	public AudioClip sewingLillebrorSound;
+	public AudioClip haveLillebrorSound;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +18,11 @@ public class PuzzleLillebror : MonoBehaviour {
 	{
 		if(other.tag == "Player")
 		{
+			AudioSource soundSource = GameObject.Find("HeadAudioSource").transform.audio;
 			if(other.transform.GetComponent<Inventory>().lillebrorComplete)
 			{
+				soundSource.clip = haveLillebrorSound;
+				soundSource.Play();
 				Door.isLocked = false;
 			}
 			else
@@ -29,6 +36,13 @@ public class PuzzleLillebror : MonoBehaviour {
 						objectiveController.multipleTask = false;
 					}
 				}
+
+				if(needLillebrorSound.Length > 0)
+				{
+					soundSource.clip = needLillebrorSound[Random.Range(0, needLillebrorSound.Length)];
+					soundSource.Play();
+				}
+
 				Debug.Log("You do not have lillebror collected yet");
 			}
 		}
