e218c341fa021846dd86063f9fe7396fbb68b0f6
Miki Anthony <miki_anthony@hotmail.com>
2015-03-29 14:12:54 +0200

    Darkness trigger script

---
+++ UnityProject/Assets/Scripts/Triggers/DarknessLevel1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarknessLevel1 : MonoBehaviour {
+
+	public Transform normalLights;
+	public Transform horrorLights;
+	public Camera firstPersonCam;
+	public Camera thirdPersonCam;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(this);
+			normalLights.gameObject.SetActive(false);
+			horrorLights.gameObject.SetActive(true);
+			firstPersonCam.clearFlags = CameraClearFlags.SolidColor;
+			firstPersonCam.backgroundColor = Color.black;
+			thirdPersonCam.clearFlags = CameraClearFlags.SolidColor;
+			thirdPersonCam.backgroundColor = Color.black;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
