63139f20784a2798806f7acb10a0ee1ad71f89db
Miki Anthony <miki_anthony@hotmail.com>
2015-04-22 17:48:49 +0200

    Inventory List, Key (Box close to the guitar) pick up and open door. (Interactions can only be done in third person, which needs to be fixed)

---
+++ UnityProject/Assets/Scripts/Interaction System/PickUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp :IInteractableObject 
+{
+	public override void Activate()
+	{
+		interactionIsActive = !interactionIsActive;
+		
+		GameObject siljaGO = GameObject.FindGameObjectWithTag("Player");
+		Inventory siljaInventory = siljaGO.GetComponent<Inventory>();
+
+		siljaInventory.putInInventory(this.transform.name);
+
+		Destroy(this.transform.gameObject);
+	}
+}
