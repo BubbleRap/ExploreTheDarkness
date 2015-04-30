f4c2215d11fa9a9f58525427825232dd8fbea142
Victor Apihtin <vapyhtin@gmail.com>
2015-04-30 14:30:29 +0200

    playing with eyes

---
+++ UnityProject/Assets/Scripts/Effects/BilboardObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BilboardObject : MonoBehaviour 
+{
+	public Transform lookAtTarget;
+
+	void Update () 
+	{
+		transform.LookAt(lookAtTarget);
+	}
+
+	void OnBecameVisible()
+	{
+		Debug.Log("Became Visible");
+	}
+
+	void OnBecameInvisible()
+	{
+		Debug.Log("Became Invisible");
+	}
+}
