a1a0a80d4c858567576485d62ca3ae852d1bc769
Victor Apihtin <vapyhtin@gmail.com>
2015-07-03 21:32:56 +0200

    Highlight Occlusion

--- UnityProject/Assets/Scripts/Effects/HighlightCamera.cs
+++ UnityProject/Assets/Scripts/Effects/HighlightCamera.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 using UnityStandardAssets.ImageEffects;
 using UnityEngine.Rendering;
 
 public class HighlightCamera : MonoBehaviour 
 {
+	public enum OcclusionType
+	{
+		Occludee,
+		Occluder
+	}
+
 	private Camera m_camera = null;
 	private Material m_highlightShader = null;
 	private BlurOptimized m_blur = null;
@@ -16,6 +22,9 @@ public class HighlightCamera : MonoBehaviour
 	private Material m_drawMaterial;
 
 	private CommandBuffer m_renderBuffer;
+	private CommandBuffer m_occlusionBuffer;
+
+	private Renderer[] m_occluders = null;
 
 	private void Awake()
 	{
@@ -34,6 +43,9 @@ public class HighlightCamera : MonoBehaviour
 
 		m_renderBuffer = new CommandBuffer();
 		m_camera.AddCommandBuffer( CameraEvent.BeforeImageEffects, m_renderBuffer );
+
+		m_occlusionBuffer = new CommandBuffer();
+		m_camera.AddCommandBuffer( CameraEvent.AfterImageEffects, m_occlusionBuffer );
 	}
 
 	public void SetSelectionColor( Color col )
@@ -41,10 +53,30 @@ public class HighlightCamera : MonoBehaviour
 		m_drawMaterial.SetColor( "_Color", col );
 	}
 
+	public void SetOccluderObjectsTag( string tag )
+	{
+		if( string.IsNullOrEmpty(tag) )
+			return;
+
+		GameObject[] occluderGOs = GameObject.FindGameObjectsWithTag(tag);
+
+		List<Renderer> occluders = new List<Renderer>();
+		foreach( GameObject go in occluderGOs )
+		{
+			Renderer renderer = go.GetComponent<Renderer>();
+			if( renderer != null )
+				occluders.Add( renderer );
+		}
+
+		m_occluders = occluders.ToArray();
+	}
+
 	private void Update()
 	{
 		m_renderBuffer.Clear();
+		m_occlusionBuffer.Clear();
 
+		// Drawing objects
 		foreach(IInteractableObject interaction in highlightObjects)
 		{
 			if( !interaction.IsActivated() )
@@ -54,8 +86,17 @@ public class HighlightCamera : MonoBehaviour
 			if( renderer == null )
 				continue;
 
-			m_renderBuffer.DrawRenderer( renderer, m_drawMaterial );
+			m_renderBuffer.DrawRenderer( renderer, m_drawMaterial, 0, (int) OcclusionType.Occludee );
+		}
 
+		// Occluding objects
+
+		if( m_occluders == null )
+			return;
+
+		foreach(Renderer renderer in m_occluders)
+		{	
+			m_occlusionBuffer.DrawRenderer( renderer, m_drawMaterial, 0, (int) OcclusionType.Occluder );
 		}
 	}
 
