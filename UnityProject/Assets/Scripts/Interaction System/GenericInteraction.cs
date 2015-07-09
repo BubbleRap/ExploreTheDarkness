ad76f62fdeca2385714dfec0d7985c13fb1b8001
Victor Apihtin <vapyhtin@gmail.com>
2015-07-09 17:09:24 +0200

    generic interaction script

---
+++ UnityProject/Assets/Scripts/Interaction System/GenericInteraction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class GenericInteraction : IInteractableObject 
+{
+	public UnityEvent m_onInteractionActivate;
+	public UnityEvent m_onInteractionDisactivate;
+
+	public void Activate()
+	{
+		interactionIsActive = !interactionIsActive;
+
+		if( interactionIsActive )
+		{
+			m_onInteractionActivate.Invoke();
+			IsCompleted = true;
+		}
+		else
+		{
+			m_onInteractionDisactivate.Invoke();
+			IsCompleted = true;
+		}
+	}
+}
