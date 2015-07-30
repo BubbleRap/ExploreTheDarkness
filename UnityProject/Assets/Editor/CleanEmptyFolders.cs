7b74bedf418380a69a7b75cd8fd34df9df8e6014
Victor Apihtin <vapyhtin@gmail.com>
2015-07-30 17:56:06 +0200

    empty folders cleaner

---
+++ UnityProject/Assets/Editor/CleanEmptyFolders.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using System;
+using System.IO;
+
+public class MenuTest : MonoBehaviour {
+
+	[MenuItem ("Utility/Remove Empty Folders")]
+	static void RemoveEmptyFolders () 
+	{
+		RemoveDirectory(Application.dataPath);
+		AssetDatabase.Refresh();
+	}
+
+	static void RemoveDirectory( string path )
+	{
+		string[] subDirs = Directory.GetDirectories( path );
+
+		foreach( string dir in subDirs )
+		{
+			RemoveDirectory( dir );
+		}
+
+		string[] files = Directory.GetFiles( path );
+		
+		if( files.Length == 0 )
+		{
+			Directory.Delete(path);
+			File.Delete(path + ".meta");
+
+			Debug.Log(path + " removed");
+		}
+	}
+}
