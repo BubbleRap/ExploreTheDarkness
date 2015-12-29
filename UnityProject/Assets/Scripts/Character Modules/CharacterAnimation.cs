9311a79aa7bf56c5d2182df5e015cce78978ed4e
Victor Apihtin <vapyhtin@gmail.com>
2015-12-29 03:10:16 +0100

    a bit of refactoring on silja behaviour

---
+++ UnityProject/Assets/Scripts/Character Modules/CharacterAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public string forwardSpeedKey = "speed";
+    public string sidewaysSpeedKey = "sidespeed";
+    public string turningSpeedKey = "turningspeed";
+
+    private Animator m_animator;
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+  //      m_animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, leftFootPositionWeight);
+    //    m_animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, leftFootRotationWeight);
+
+      //  m_animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootObj.position);
+        //m_animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootObj.rotation);
+    }
+
+    public void SetForwardSpeed(float speed)
+    {
+        m_animator.SetFloat(forwardSpeedKey, speed);
+    }
+
+    public void SetSidewaysSpeed(float speed)
+    {
+        m_animator.SetFloat(sidewaysSpeedKey, speed);
+    }
+
+    public void SetTurningSpeed(float speed)
+    {
+        m_animator.SetFloat(turningSpeedKey, speed);
+    }
+}
