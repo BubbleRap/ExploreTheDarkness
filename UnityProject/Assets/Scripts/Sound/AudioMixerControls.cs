eb224d43b20f52002d2507fa5704fc2367f67ada
Victor Apihtin <vapyhtin@gmail.com>
2016-01-18 12:34:29 +0100

    audio mixer controls: snapshots

--- UnityProject/Assets/Scripts/Sound/AudioMixerControls.cs
+++ UnityProject/Assets/Scripts/Sound/AudioMixerControls.cs
@@ -4,20 +4,43 @@ using UnityEngine.Audio;
 
 public class AudioMixerControls : MonoBehaviour 
 {
+    public enum MixingType
+    {
+        Parameter,
+        Snapshot
+    }
+
+    public MixingType m_mixingType;
+
 	public float m_minValue = -20f;
 	public float m_maxValue = 80f;
 	public string m_paramName = "Master";
+
+    public string m_snapshotName;
+    private AudioMixerSnapshot m_snapshot;
+
 	public float m_time = 1f;
 
 	public AudioMixer m_audioMixer;
-	public AudioSource m_audioSource;
+//	public AudioSource m_audioSource;
 
 	void Awake()
 	{
-		if( m_audioMixer == null )
-			Debug.LogError( gameObject.name + " AudioMixer is not assigned" );
-		if( m_audioSource == null )
-			Debug.LogError( gameObject.name + " AudioSource is not assigned" );
+        if( m_mixingType == MixingType.Parameter )
+        {
+		    if( m_audioMixer == null )
+			    Debug.LogError( gameObject.name + " AudioMixer is not assigned" );
+        }
+
+        else
+        {
+            if( !string.IsNullOrEmpty(m_snapshotName) )
+                m_snapshot = m_audioMixer.FindSnapshot(m_snapshotName);
+            else
+                Debug.LogError( gameObject.name + " Snapshot name isn't assigned" );
+        }
+        //      if( m_audioSource == null )
+        //          Debug.LogError( gameObject.name + " AudioSource is not assigned" );
 	}
 
 	public void FadeInAudio()
@@ -32,10 +55,15 @@ public class AudioMixerControls : MonoBehaviour
 		StartCoroutine(FadeOut());
 	}
 
+    public void FadeToSnapshot()
+    {
+        m_snapshot.TransitionTo(m_time);   
+    }
+
 	private IEnumerator FadeIn( )
 	{
-				if( m_audioSource != null )
-		m_audioSource.enabled = true;
+//		if( m_audioSource != null )
+//		    m_audioSource.enabled = true;
 		m_audioMixer.SetFloat(m_paramName, m_minValue);
 		float timer = 0f;
 
@@ -62,7 +90,7 @@ public class AudioMixerControls : MonoBehaviour
 		}
 
 		m_audioMixer.SetFloat(m_paramName, m_minValue);
-				if( m_audioSource != null )
-		m_audioSource.enabled = false;
+//		if( m_audioSource != null )
+//		    m_audioSource.enabled = false;
 	}
 }
