bd66e1e486bf1ad04bfeb6ade3b527b08c80593d
Victor Apihtin <vapyhtin@gmail.com>
2016-01-18 14:36:51 +0100

    audio mixer editor script

---
+++ UnityProject/Assets/Scripts/Editor/AudioMixerControlsEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AudioMixerControls))]
+public class AudioMixerControlsEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        AudioMixerControls myTarget = target as AudioMixerControls;
+
+        myTarget.m_mixingType = (AudioMixerControls.MixingType) EditorGUILayout.EnumPopup("Mixing type",  myTarget.m_mixingType);
+
+        if(myTarget.m_mixingType == AudioMixerControls.MixingType.FadeInOutParameter)
+        {
+            myTarget.m_minValue = EditorGUILayout.FloatField("Min Volume", myTarget.m_minValue);
+            myTarget.m_maxValue = EditorGUILayout.FloatField("Max Volume", myTarget.m_maxValue);
+            myTarget.m_paramName = EditorGUILayout.TextField("Parameter Name", myTarget.m_paramName);
+
+            myTarget.m_audioSource = (AudioSource) EditorGUILayout.ObjectField(myTarget.m_audioSource, typeof(AudioSource), true);
+        }
+
+        if( myTarget.m_mixingType == AudioMixerControls.MixingType.FadeToSnapshot )
+        {
+            myTarget.m_snapshotName = EditorGUILayout.TextField("Snapshot Name", myTarget.m_snapshotName);
+        }
+
+        if( myTarget.m_mixingType == AudioMixerControls.MixingType.PlayOneShot )
+        {
+            myTarget.m_audioSource = (AudioSource) EditorGUILayout.ObjectField(myTarget.m_audioSource, typeof(AudioSource), true);
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Sound/AudioMixerControls.cs
+++ UnityProject/Assets/Scripts/Sound/AudioMixerControls.cs
@@ -6,41 +6,52 @@ public class AudioMixerControls : MonoBehaviour
 {
     public enum MixingType
     {
-        Parameter,
-        Snapshot
+        PlayOneShot,
+        FadeInOutParameter,
+        FadeToSnapshot
     }
 
     public MixingType m_mixingType;
 
+    public float m_volume = 1f;
+
 	public float m_minValue = -20f;
 	public float m_maxValue = 80f;
-	public string m_paramName = "Master";
+	public string m_paramName = "My Exposed Parameter";
 
-    public string m_snapshotName;
-    private AudioMixerSnapshot m_snapshot;
+    public string m_snapshotName = "My Snapshot";
+   
 
 	public float m_time = 1f;
 
 	public AudioMixer m_audioMixer;
-//	public AudioSource m_audioSource;
+	public AudioSource m_audioSource;
+
+    private AudioMixerSnapshot m_snapshot = null;
 
 	void Awake()
 	{
-        if( m_mixingType == MixingType.Parameter )
+        if( m_mixingType == MixingType.PlayOneShot )
+        {
+            if( m_audioSource == null )
+                Debug.LogError( gameObject.name + " AudioSource is not assigned" ); 
+        }
+
+        if( m_mixingType == MixingType.FadeInOutParameter )
         {
 		    if( m_audioMixer == null )
 			    Debug.LogError( gameObject.name + " AudioMixer is not assigned" );
+            if( m_audioSource == null )
+                Debug.LogError( gameObject.name + " AudioSource is not assigned" ); 
         }
 
-        else
+        if( m_mixingType == MixingType.FadeToSnapshot )
         {
             if( !string.IsNullOrEmpty(m_snapshotName) )
                 m_snapshot = m_audioMixer.FindSnapshot(m_snapshotName);
             else
                 Debug.LogError( gameObject.name + " Snapshot name isn't assigned" );
         }
-        //      if( m_audioSource == null )
-        //          Debug.LogError( gameObject.name + " AudioSource is not assigned" );
 	}
 
 	public void FadeInAudio()
@@ -55,15 +66,25 @@ public class AudioMixerControls : MonoBehaviour
 		StartCoroutine(FadeOut());
 	}
 
+    public void PlayAudioSource()
+    {
+        if( m_audioSource != null )
+            m_audioSource.enabled = true;
+
+        StopAllCoroutines();
+        StartCoroutine(PlayOneShot());
+    }
+
     public void FadeToSnapshot()
     {
         m_snapshot.TransitionTo(m_time);   
     }
 
-	private IEnumerator FadeIn( )
+	private IEnumerator FadeIn()
 	{
-//		if( m_audioSource != null )
-//		    m_audioSource.enabled = true;
+		if( m_audioSource != null )
+		    m_audioSource.enabled = true;
+
 		m_audioMixer.SetFloat(m_paramName, m_minValue);
 		float timer = 0f;
 
@@ -77,7 +98,7 @@ public class AudioMixerControls : MonoBehaviour
 		m_audioMixer.SetFloat(m_paramName, m_maxValue);
 	}
 	
-	private IEnumerator FadeOut( )
+	private IEnumerator FadeOut()
 	{
 		m_audioMixer.SetFloat(m_paramName, m_maxValue);
 		float timer = 0f;
@@ -90,7 +111,19 @@ public class AudioMixerControls : MonoBehaviour
 		}
 
 		m_audioMixer.SetFloat(m_paramName, m_minValue);
-//		if( m_audioSource != null )
-//		    m_audioSource.enabled = false;
+
+		if( m_audioSource != null )
+		    m_audioSource.enabled = false;
 	}
+
+    private IEnumerator PlayOneShot()
+    {
+        m_audioSource.volume = m_volume;
+        m_audioSource.Play();
+
+        yield return new WaitForSeconds(m_audioSource.clip.length);
+
+        if( m_audioSource != null )
+            m_audioSource.enabled = false;
+    }
 }
