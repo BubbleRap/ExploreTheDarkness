ef8785827e36a4c781564eef4ebf47babfd30188
Miki Anthony <miki_anthony@hotmail.com>
2016-02-26 13:22:39 +0100

    Intro text dialog sequence ("Twine")

---
+++ UnityProject/Assets/Scripts/DialogSystem/Dialog.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Dialog {
+
+	[TextArea(3,10)]
+	public string Text;
+	public Option[] options;
+}
