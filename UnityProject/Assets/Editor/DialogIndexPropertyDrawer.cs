a787520bd98f3d0d864f6dee43e0fcff0337ae5c
Victor Apihtin <vapyhtin@gmail.com>
2016-03-08 23:05:07 +0100

    the most crazy script i have written in my entire life [for mia]

---
+++ UnityProject/Assets/Editor/DialogIndexPropertyDrawer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer (typeof (Indexer))]
+public class DialogIndexPropertyDrawer : PropertyDrawer {
+
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) 
+    {
+        EditorGUI.BeginProperty (position, label, property);
+
+        position = EditorGUI.PrefixLabel (position, GUIUtility.GetControlID (FocusType.Passive), label);
+
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var amountRect = new Rect (position.x, position.y, 30, position.height);
+
+        GUI.SetNextControlName(label.text);
+
+        SerializedProperty propertyItem = property.FindPropertyRelative ("index");
+        EditorGUI.PropertyField (amountRect, propertyItem, GUIContent.none);
+
+        if( KeyPressed(label.text, KeyCode.Return))
+        {
+            DialogChoices dialogSystem = property.serializedObject.targetObject as DialogChoices;
+            dialogSystem.SortDialogsByIndex();
+        }
+
+        EditorGUI.indentLevel = indent;
+
+
+        EditorGUI.EndProperty ();
+    }
+
+    private bool KeyPressed(string controlName, KeyCode key)
+    {
+        if(GUI.GetNameOfFocusedControl() == controlName)
+        {
+            if ((Event.current.type == EventType.KeyUp) && 
+                (Event.current.keyCode == key))
+
+                return true;
+
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- UnityProject/Assets/Scripts/DialogSystem/Dialog.cs
+++ UnityProject/Assets/Scripts/DialogSystem/Dialog.cs
@@ -3,9 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 
 [System.Serializable]
-public class Dialog {
-
+public class Dialog 
+{
+    public Indexer index;
 	[TextArea(3,10)]
 	public string Text;
 	public Option[] options;
+}
+
+[System.Serializable]
+public class Indexer
+{
+    public int index;
 }
