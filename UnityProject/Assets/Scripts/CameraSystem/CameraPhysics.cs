dc3cedee6ef58d43952d76683635a5b594a2f19a
Michal Krolikowski <hiszpanskainkwizycja@gmail.com>
2016-04-03 19:04:43 +0200

    Different approach to TPP camera collision detection - WIP

--- UnityProject/Assets/Scripts/CameraSystem/CameraPhysics.cs
+++ UnityProject/Assets/Scripts/CameraSystem/CameraPhysics.cs
@@ -1,73 +1,135 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
-[RequireComponent(typeof(Collider))] 
+[RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CameraFollow))]
 public class CameraPhysics : MonoBehaviour 
 {
 	// Percents
-	[Range(0f, 1f)]
+	[Range(0f, 10f)]
+    [Tooltip("In units per second")]
 	public float approachingSpeed = 0.1f;
 
 	public AnimationCurve fallbackCurve;
 	[Range(0f, 5f)]
 	public float fallbackTime = 5f;
 
-	private CameraFollow follower = null;
+    public Transform HeadBone;
 
-	private List<Collider> colliders = new List<Collider>();
+    [Header("Debug")]
+    public bool hasHit;
+    public float currentHitDistance;
+
+    private CameraFollow follower = null;
+
+    private RaycastHit[] raycasts = new RaycastHit[1];
 
 	private float _timer = 5f;
 	private float fallbackFrom = 0f;
+    private List<Collider> collidingColliders = new List<Collider>();
 
-	void Awake()
+    void Awake()
 	{
 		follower = GetComponent<CameraFollow> ();
-	}
+    }
 
-	void Update()
+    void Update()
 	{
+
 		_timer += Time.deltaTime;
 		_timer = Mathf.Clamp(_timer, 0f, fallbackTime);
 
-		RaycastHit hit = default(RaycastHit);
+        hasHit = CheckForHits();
 
-		// if camera collides itself
-		if( colliders.Count > 0 
-		   // if camera center point raycast hits a wall
-	//	   ||  	Physics.Raycast(transform.position, transform.forward, out hit, transform.localPosition.magnitude, 1 << 0 ) 
-		   // if raycast to character hits a wall
-		   ||	Physics.Raycast(transform.position, (follower.cameraFocusTarget.position - transform.position).normalized, out hit, follower.cameraDistance, 1 << 0) 
-		   )
+		if(hasHit)
 		{
-			// if there is a hit, then get a close up
-			follower.cameraDistance = Mathf.Lerp (follower.cameraDistance, follower.maxDistance * follower.minDistance, approachingSpeed);
+            currentHitDistance = GetMaxDistance();
 
+            if (currentHitDistance < follower.cameraDistance)
+            {
+                // if there is a hit, then get a close up
+                follower.cameraDistance =
+                    Mathf.Lerp(
+                        follower.cameraDistance,
+                        currentHitDistance,
+                        approachingSpeed * Time.deltaTime //* ((collidingColliders.Count > 0) ? 2 : 1)
+                );
 
-			_timer = 0f;
-			fallbackFrom = follower.cameraDistance;
-		}
-		else
-		{
-			if( _timer < fallbackTime )
-			{
-				// fallback the camera if there is no collision happening
-				follower.cameraDistance = Mathf.Lerp (fallbackFrom, follower.maxDistance, fallbackCurve.Evaluate(_timer / fallbackTime ) );
-			}
-		}
+                _timer = 0f;
+                fallbackFrom = follower.cameraDistance;
+            }
+            else
+            {
+                if (_timer < fallbackTime)
+                {
+                    // fallback the camera if there is no collision happening
+                    follower.cameraDistance = Mathf.Lerp(fallbackFrom, currentHitDistance, fallbackCurve.Evaluate(_timer / fallbackTime));
+                }
+            }
+        }
+        else
+        {
+            currentHitDistance = follower.maxDistance;
 
-	}
+            if (_timer < fallbackTime)
+            {
+                // fallback the camera if there is no collision happening
+                follower.cameraDistance = Mathf.Lerp(fallbackFrom, follower.maxDistance, fallbackCurve.Evaluate(_timer / fallbackTime));
+            }
+        }
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-	//	Debug.Log(other.name);
+    private float GetMaxDistance()
+    {
+        return Mathf.Max(
+            follower.minDistance,
+                Mathf.Min(
+                follower.maxDistance,
+                Vector3.Distance(
+                    HeadBone.position,
+                    raycasts[0].point
+                ) - 0.2f
+            )
+        );
+    }
 
-		colliders.Add (other);
-	}
-	
-	void OnTriggerExit(Collider other)
-	{
-		colliders.Remove (other);
-	}
+    private bool CheckForHits()
+    {
+        raycasts[0] = default(RaycastHit);
+
+        bool hasHit = 
+            Physics.Raycast(
+               HeadBone.position,
+                -transform.forward,
+                out raycasts[0],
+                follower.maxDistance,
+                ((1 << LayerMask.NameToLayer("Default")))
+        );
+
+        return hasHit;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawLine(
+                HeadBone.position,
+                ((raycasts[0].transform != null) ? raycasts[0].point :
+                HeadBone.position + transform.forward * -follower.maxDistance));
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.layer == LayerMask.NameToLayer("Default"))
+            collidingColliders.Add(c);
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.layer == LayerMask.NameToLayer("Default"))
+            collidingColliders.Remove(c);
+    }
 }
