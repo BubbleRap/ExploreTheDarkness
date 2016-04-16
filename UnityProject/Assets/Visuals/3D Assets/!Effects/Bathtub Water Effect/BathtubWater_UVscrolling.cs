e935aa2830cdc5d145ae707923be844627f0c01f
QuasiHobo <quasihobo@gmail.com>
2016-04-16 17:33:35 +0200

    TrashBin + minor fixes

---
+++ UnityProject/Assets/Visuals/3D Assets/!Effects/Bathtub Water Effect/BathtubWater_UVscrolling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BathtubWater_UVscrolling : MonoBehaviour {
+
+	Renderer myRenderer; 
+	public float animSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRenderer = this.gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float offset = Time.time * animSpeed;
+		myRenderer.material.mainTextureOffset = new Vector2(offset, 0);
+
+		myRenderer.material.SetTextureOffset("_BumpMap", new Vector2(offset, 0));
+	}
+}
---
+++ UnityProject/Assets/Visuals/3D Assets/Scripts/CandleLight_Pointlight_Behavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[ExecuteInEditMode]
+public class CandleLight_Pointlight_Behavior : MonoBehaviour {
+
+	public Light candleLight;
+	float randomValue;
+	public float minIntensity;
+	public float maxIntensity;
+	public float flickerSpeed;
+
+	// Use this for initialization
+	void Start () {
+		candleLight = this.gameObject.GetComponent<Light>();
+		randomValue = Random.Range(0.0f, 65535.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float noise = Mathf.PerlinNoise(randomValue, Time.time*flickerSpeed);
+		candleLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+	}
+}
