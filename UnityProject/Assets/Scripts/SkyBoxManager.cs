22f3f0590eded755e8e8bf2c345cc5d6e1174d1f
QuasiHobo <quasihobo@gmail.com>
2016-04-17 17:45:22 +0200

    AGAIN HAHAHAHA HEY VICTOR!!!

--- UnityProject/Assets/Scripts/SkyBoxManager.cs
+++ UnityProject/Assets/Scripts/SkyBoxManager.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class SkyBoxManager : MonoBehaviour {
 
-	float lightExposure = 0.9f;
-	float darkExposure = 0.2f;
+	public float lightExposure = 0.9f;
+	public float darkExposure = 0.2f;
+
+	void Start()
+	{
+		RenderSettings.skybox.SetFloat("_Exposure", lightExposure);
+	}
+
+	void OnDisable()
+	{
+		RenderSettings.skybox.SetFloat("_Exposure", lightExposure);
+	}
 
 	public void LightMode()
 	{
