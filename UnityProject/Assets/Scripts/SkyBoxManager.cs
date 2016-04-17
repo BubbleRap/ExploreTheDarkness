848a15e1712e83700795306d6709113926fa6898
QuasiHobo <quasihobo@gmail.com>
2016-04-17 17:34:31 +0200

    Skybox changes + Flashlight tweaks

---
+++ UnityProject/Assets/Scripts/SkyBoxManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyBoxManager : MonoBehaviour {
+
+	float lightExposure = 0.9f;
+	float darkExposure = 0.2f;
+
+	public void LightMode()
+	{
+		RenderSettings.skybox.SetFloat("_Exposure", lightExposure);
+	}
+
+	public void DarkMode()
+	{
+		RenderSettings.skybox.SetFloat("_Exposure", darkExposure);
+	}
+
+}
