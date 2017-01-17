5b99d172c31dc067e13716b1cc79406c0d87d106
Victor Apihtin <vapyhtin@gmail.com>
2017-01-17 20:47:08 +0100

    damn folder names yo

--- UnityProject/Assets/PostProcessing/Editor/Utils/EditorResources.cs
+++ UnityProject/Assets/PostProcessing/Editor/Utils/EditorResources.cs
@@ -19,7 +19,7 @@ namespace UnityEditor.PostProcessing
                     if (SearchForEditorResourcesPath(out path))
                         m_EditorResourcesPath = path;
                     else
-                        Debug.LogError("Unable to locate editor resources. Make sure the PostProcessing package has been installed correctly.");
+                        Debug.LogError("Unable to locate editor resources at: " + path);
                 }
 
                 return m_EditorResourcesPath;
